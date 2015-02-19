af767a9eed15e087fca42c5e8fda24dd3174564b
Daniel Mané <danmane@gmail.com>
2015-02-18 21:00:45 -0800

    Remove things I hopefully won't need

--- sidious/main.go
+++ sidious/main.go
@@ -15,44 +15,6 @@ func bustaMove(s game.State, limit time.Duration) game.State {
 	return *exhale(move)
 }
 
-// func inhale(s *game.State) *implgame.State {
-// 	return &implgame.State{
-// 		Board:          inhaleB(&s.Board),
-// 		NextPlayer:     implgame.Player(s.NextPlayer),
-// 		MovesRemaining: s.MovesRemaining,
-// 		MarblesPerMove: s.MarblesPerMove,
-// 		LossThreshold:  s.LossThreshold,
-// 	}
-// }
-// func exhale(s *implgame.State) *game.State {
-// 	return &game.State{
-// 		Board:          exhaleB(&s.Board),
-// 		NextPlayer:     game.Player(s.NextPlayer),
-// 		MovesRemaining: s.MovesRemaining,
-// 		MarblesPerMove: s.MarblesPerMove,
-// 		LossThreshold:  s.LossThreshold,
-// 	}
-// }
-
-// func exhaleB(b *implgame.Board) game.Board {
-// 	return game.Board{
-// 		WhitePositions: game.HexSet(b.WhitePositions),
-// 		BlackPositions: game.HexSet(b.BlackPositions),
-// 		EdgeLength:     b.EdgeLength,
-// 	}
-// }
-// func inhaleB(b *game.Board) implgame.Board {
-// 	return implgame.Board{
-// 		WhitePositions: implgame.HexSet(b.WhitePositions),
-// 		BlackPositions: implgame.HexSet(b.BlackPositions),
-// 		EdgeLength:     b.EdgeLength,
-// 	}
-// }
-
-// func inhaleHS (hs game.HexSet) implgame.HexSet {
-
-// }
-
 var (
 	port = flag.String("port", "3423", "port the ai runs on")
 )
