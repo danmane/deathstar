0fccdbbe4ed3068afd8afb8b630b9b947d3693e6
Daniel Mané <danmane@gmail.com>
2015-02-22 11:03:31 -0800

    Add some bitvector vs int perf comparisons

--- implgame/hexIndexArray_test.go
+++ implgame/hexIndexArray_test.go
@@ -32,3 +32,54 @@ func Test_conversion(t *testing.T) {
 		t.Error("conversion from", standardWhitePositions, "to", converted, "failed")
 	}
 }
+
+func hia2int(arr [61]bool) uint64 {
+	var out uint64 = 0
+	var i uint64
+	for i = 0; i < 61; i++ {
+		if arr[i] {
+			out |= (1 << i)
+		}
+	}
+	return out
+}
+
+func Benchmark_HIA_copy(b *testing.B) {
+	var CopiedHIA [61]bool
+	a := standardWhitePositions.arr
+	for n := 0; n <= b.N; n++ {
+		CopiedHIA = a
+	}
+	a = CopiedHIA
+}
+
+func Benchmark_int_copy(b *testing.B) {
+	var CopiedInt uint64
+	a := hia2int(standardWhitePositions.arr)
+	for n := 0; n <= b.N; n++ {
+		CopiedInt = a
+	}
+	a = CopiedInt
+}
+
+func Benchmark_HIA_ToSlice(b *testing.B) {
+	a := standardWhitePositions
+	for n := 0; n <= b.N; n++ {
+		a.ToSlice()
+	}
+}
+
+func Benchmark_int_ToSlice(b *testing.B) {
+	a := hia2int(standardWhitePositions.arr)
+	for n := 0; n <= b.N; n++ {
+		out := make([]Hex, standardWhitePositions.length)
+		x := 0
+		var i uint64 = 0
+		for i = 0; i < 61; i++ {
+			if (a>>i)&1 == 1 {
+				out[x] = *fromIdx(int(i))
+				x++
+			}
+		}
+	}
+}
