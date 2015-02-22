a2f6f3c6e6eb22811a48cdf2eb5878c9c8bc0494
Daniel Mané <danmane@gmail.com>
2015-02-22 05:19:52 -0800

    Stop invoking pieces.ToSlice (needless allocs for intermediate data struct)

--- implgame/segment.go
+++ implgame/segment.go
@@ -20,27 +20,30 @@ func (s *Segment) segPieces() []Hex {
 func (g *State) Segments(p Player) []Segment {
 	pieces := g.Board.Pieces(p)
 	result := make([]Segment, 0, 3*pieces.length)
-	for _, pos := range pieces.ToSlice() {
-		s := Segment{
-			base:        pos,
-			Length:      1,
-			player:      p,
-			orientation: NullDirection,
-		}
-		result = append(result, s)
-		for d := TopRight; d <= BotRight; d++ {
-			next := pos.adjacent(d)
-			length := 2
-			for length <= g.MarblesPerMove && pieces.has(next) {
-				s = Segment{
-					base:        pos,
-					orientation: d,
-					Length:      length,
-					player:      p,
+	for i := 0; i < 61; i++ {
+		if pieces.arr[i] {
+			pos := *fromIdx(i)
+			s := Segment{
+				base:        pos,
+				Length:      1,
+				player:      p,
+				orientation: NullDirection,
+			}
+			result = append(result, s)
+			for d := TopRight; d <= BotRight; d++ {
+				next := pos.adjacent(d)
+				length := 2
+				for length <= g.MarblesPerMove && pieces.has(next) {
+					s = Segment{
+						base:        pos,
+						orientation: d,
+						Length:      length,
+						player:      p,
+					}
+					next = next.adjacent(d)
+					length++
+					result = append(result, s)
 				}
-				next = next.adjacent(d)
-				length++
-				result = append(result, s)
 			}
 		}
 	}
